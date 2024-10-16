6548bad5502232f6123f90cc31d9c5c8f28eef36
Macflash <the.alex.breyfogle@gmail.com>
2024-10-15 18:27:19 -0700

    Add freezer section

--- src/food/pantry.tsx
+++ src/food/pantry.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import { Eatable } from "./eatable";
 import { POWDERS } from "./eatables/powders";
+import { FREEZER } from "./eatables/freezer";
 
 export function Pantry({ onAdd }: { onAdd?: (added: Eatable) => void }) {
   return (
     <div>
-      <Collapsable title="Powders" startExpanded>
-        {POWDERS.map((p) => (
-          <div style={{ backgroundColor: "#EEE", margin: "5px 0" }}>
-            {onAdd ? (
-              <button style={{ marginRight: 5 }} onClick={() => onAdd?.(p)}>
-                +
-              </button>
-            ) : undefined}
-            <span style={{ fontWeight: 600 }}>{p.brand}</span>
-            {(p.brand ? " " : "") + p.name}
-          </div>
-        ))}
-      </Collapsable>
+      <PantrySection title="Powders" eatables={POWDERS} onAdd={onAdd} />
+      <PantrySection title="Freezer" eatables={FREEZER} onAdd={onAdd} />
     </div>
+  );
+}
+
+function PantrySection({
+  onAdd,
+  eatables,
+  title,
+}: {
+  title: string;
+  eatables: ReadonlyArray<Eatable>;
+  onAdd?: (added: Eatable) => void;
+}) {
+  return (
+    <Collapsable title={title} startExpanded>
+      {eatables.map((p) => (
+        <div style={{ backgroundColor: "#EEE", margin: "5px 0" }}>
+          {onAdd ? (
+            <button style={{ marginRight: 5 }} onClick={() => onAdd?.(p)}>
+              +
+            </button>
+          ) : undefined}
+          <span style={{ fontWeight: 600 }}>{p.brand}</span>
+          {(p.brand ? " " : "") + p.name}
+        </div>
+      ))}
+    </Collapsable>
   );
 }
 
